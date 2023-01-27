a07b6322f5628f3de5e7e53e44bb996350b033c9
Marcoul Robin <marcoul.robin@etu.minesparis.psl.eu>
2023-01-27 10:18:14 +0100

    ajout salles

--- plateau.py
+++ plateau.py
@@ -3,9 +3,24 @@ import numpy as np
 class plateau :
 
     def __init__(self, dim) :
-        _dim = dim
-        _plat = [[' ' for k in range(_dim)] for k in range(_dim)]
+        self._dim = dim
+        self._plat = np.zeros((self._dim,self._dim))
+
+    def creer_salle(self, origine, dim) :
+        self._plat[origine[0],origine[1]:origine[1]+dim[1]] = 1
+        self._plat[origine[0]+dim[0]-1,origine[1]:origine[1]+dim[1]] = 1
+        self._plat[origine[0]:origine[0]+dim[0],origine[1]] = 1
+        self._plat[origine[0]:origine[0]+dim[0],origine[1]+dim[1]-1] = 1
+
+    def afficher(self) :
+        print(self._plat)
 
 
-plateau(4)
+
+p = plateau(20)
+p.creer_salle((1,1),(5,5))
+p.creer_salle((6,7),(4,5))
+p.afficher()
+
+
 
