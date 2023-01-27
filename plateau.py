333b3e010ae6034e69a044b6eec2b9139f4da0e2
Agathe Chauvel <agathe.chauvel@etu.minesparis.psl.eu>
2023-01-27 10:56:20 +0100

    window v1 Agathe

---
+++ plateau.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class plateau :
+
+    def __init__(self, dim) :
+        _dim = dim
+        _plat = [[' ' for k in range(_dim)] for k in range(_dim)]
+
+
+plateau(4)
+
+        
+
